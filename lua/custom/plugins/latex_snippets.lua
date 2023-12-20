18af02d399be35debb046e92dfe7e48860505d5f
Alex Bustos <bustosalex1@gmail.com>
2023-12-19 22:33:09 -0500

    trying to get better latex snippets

--- lua/custom/plugins/latex_snippets.lua
+++ lua/custom/plugins/latex_snippets.lua
@@ -2,6 +2,7 @@ return {
   'iurimateus/luasnip-latex-snippets.nvim',
   -- vimtex isn't required if using treesitter
   requires = { 'L3MON4D3/LuaSnip' },
+  ft = { 'tex', 'plaintex', 'markdown' },
   config = function()
     require('luasnip-latex-snippets').setup { use_treesitter = true }
   end,
